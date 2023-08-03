18f6b0837eb08565fb4aeb2d9edf909a06dc2339
Otto F. Quintanilha <otto@mobilesaude.com.br>
2023-08-02 22:30:37 -0300

    test: add trending endpoint

--- src/__test__/podcasts.test.ts
+++ src/__test__/podcasts.test.ts
@@ -69,4 +69,126 @@ describe("podcasts api", () => {
       expect(searchResult.feed).toEqual(feedByItunesId);
     });
   });
+
+  describe("trending", () => {
+    it("returns a default of 40 feeds", async () => {
+      const recentResults = await client.trending();
+      expect(recentResults.feeds).toHaveLength(40);
+    });
+
+    it("returns a user provided max count of trending feeds", async () => {
+      const max = 20;
+      const recentResults = await client.trending({ max });
+      expect(recentResults.feeds).toHaveLength(max);
+    });
+
+    it("returns trending feeds based on an optional language", async () => {
+      const lang = "en";
+      const recentResults = await client.trending({ lang });
+
+      expect(recentResults.feeds.every((f) => f.language === lang)).toEqual(true);
+    });
+
+    it("returns trending feeds based on multiple optional languages", async () => {
+      const lang = ["en", "en-us"];
+      const recentResults = await client.trending({ lang });
+      expect(recentResults.feeds.every((f) => lang.includes(f.language))).toEqual(true);
+    });
+
+    it("returns returns trending feeds based on since", async () => {
+      const latestResults = await client.trending({ max: 5 });
+      const firstResults = await client.trending({
+        since: latestResults.feeds[0].newestItemPublishTime - 1,
+      });
+      const futureResults = await client.trending({
+        since: latestResults.feeds[0].newestItemPublishTime,
+      });
+
+      expect(latestResults.feeds).toHaveLength(5);
+      expect(futureResults.feeds).toHaveLength(40);
+      expect(firstResults.feeds).toHaveLength(40);
+    });
+
+    it("returns returns trending feeds based on single category", async () => {
+      const latestResults = await client.trending({ max: 5 });
+
+      const [category] = latestResults.feeds.reduce<string[]>((acc, curr) => {
+        const cats = Object.values(curr.categories ?? {});
+        return acc.concat(cats);
+      }, []);
+
+      const byCategory = await client.trending({
+        category,
+      });
+
+      expect(
+        byCategory.feeds.every((f) => Object.values(f.categories ?? {}).includes(category))
+      ).toEqual(true);
+    });
+
+    it("returns returns trending feeds based on multiple categories (categories are OR'd)", async () => {
+      const latestResults = await client.trending({ max: 5 });
+
+      const [firstCat, secondCat] = Array.from(
+        new Set(
+          latestResults.feeds.reduce<string[]>((acc, curr) => {
+            const cats = Object.values(curr.categories ?? {});
+            return acc.concat(cats);
+          }, [])
+        )
+      );
+
+      const byCategory = await client.trending({
+        category: [firstCat, secondCat],
+      });
+
+      expect(
+        byCategory.feeds.every((f) => {
+          const categories = Object.values(f.categories ?? {});
+          return categories.includes(firstCat) || categories.includes(secondCat);
+        })
+      ).toEqual(true);
+    });
+
+    it("returns returns trending feeds based on single notCategory", async () => {
+      const latestResults = await client.trending({ max: 5 });
+
+      const [category] = latestResults.feeds.reduce<string[]>((acc, curr) => {
+        const cats = Object.values(curr.categories ?? {});
+        return acc.concat(cats);
+      }, []);
+
+      const byCategory = await client.trending({
+        notCategory: category,
+      });
+
+      expect(
+        byCategory.feeds.every((f) => !Object.values(f.categories ?? {}).includes(category))
+      ).toEqual(true);
+    });
+
+    it("returns returns trending feeds based on multiple notCategories", async () => {
+      const latestResults = await client.trending({ max: 5 });
+
+      const [firstCat, secondCat] = Array.from(
+        new Set(
+          latestResults.feeds.reduce<string[]>((acc, curr) => {
+            const cats = Object.values(curr.categories ?? {});
+            return acc.concat(cats);
+          }, [])
+        )
+      );
+
+      const byCategory = await client.trending({
+        notCategory: [firstCat, secondCat],
+      });
+
+      expect(
+        byCategory.feeds.every((f) => {
+          const categories = Object.values(f.categories ?? {});
+          return !categories.includes(firstCat) && !categories.includes(secondCat);
+        })
+      ).toEqual(true);
+    });
+  });
 });
