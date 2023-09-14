df70b47430fb5d521e92fff88765d175d4271ce5
Ryan Hirsch <ryan.hirsch@gmail.com>
2023-09-14 09:31:54 -0500

    the bump

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,11 +4,11 @@ set -euo pipefail
 SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PROJECT_DIR="$( cd "${SCRIPTS_DIR}" && cd .. && pwd )"
 
-if [[ -f "${PROJECT_DIR}/.env" ]]; then
-  set -o allexport
-  source "${PROJECT_DIR}/.env"
-  set +o allexport
-fi
+# if [[ -f "${PROJECT_DIR}/.env" ]]; then
+#   set -o allexport
+#   source "${PROJECT_DIR}/.env"
+#   set +o allexport
+# fi
 
 cd $PROJECT_DIR
 PACKAGE_VERSION=$(cat package.json | grep \\\"version\\\" | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]')
